c909c1524cb3503a11301789e183f166f665c01b
FarahZKhan <farahk@student.unimelb.edu.au>
2021-05-07 10:03:10 +1000

    updated script file

--- script.js
+++ script.js
@@ -2,21 +2,31 @@
 var word = "";
 var wordGuess = [];
 var wrongGuess = [];
-var allowedGuesses = 0;
+var guessBomb = 0;
 var winCount = 1;
 var guess = "";
 var dif = 0;
 
 function difficultylevel1() {
-    // this function defines the difficulty variable "var dif"
-    // and displays the startButton div
+    dif = 1;
+    document.getElementById('startButton').style.display='block';
+    document.getElementById('chooseDifficulty').style.display='none';
+}
 
+function difficultylevel2() {
+    dif = 2;
+    document.getElementById('startButton').style.display='block';
+    document.getElementById('chooseDifficulty').style.display='none';
+}
+
+function difficultylevel3() {
+    dif = 3;
+    document.getElementById('startButton').style.display='block';
+    document.getElementById('chooseDifficulty').style.display='none';
 }
 
 
-
 function wordw() {
-    // this function generates random word
     var randomWords = ["humor", "miniature", "amusing", "creepy", "fact", "risk", "verse", "land", "lumpy", "holiday", "glorious", "weigh", "brake", "pretty", "grin", "capricious", "bite-sized", "misty", "ignore", "certain", "sloppy", "dress", "true", "zonked", "observation", "action", "various", "want", "direful", "suck", "dress", "scarecrow", "judge", "madly", "quizzical", "consist", "fierce", "love", "arrest", "serve", "fit", "hug", "tan", "curve", "eatable", "tub", "race", "innocent", "open", "preach", "steady", "acoustics", "lock", "field", "arrange", "rifle", "learned", "toe", "flow", "competition", "ill-fated", "oatmeal", "match", "male", "measure", "loaf", "smile", "wrestle", "dull", "food", "locket", "bell", "beg", "strengthen", "responsible", "enchanting", "loutish", "switch", "idea", "nine", "squeamish", "pig", "bat", "dear", "trains", "owe", "frogs", "assorted", "lonely", "hurry", "natural", "sun", "snow", "obnoxious", "broken", "friend", "bright", "cake", "sour", "permit", "economic", "lovely", "quick", "van", "tempt", "apparel", "decay", "business", "adjustment", "blushing", "makeshift", "slippery", "load", "winter", "exist", "tongue", "country", "roll", "fast", "moor", "possess", "pat", "pass", "books", "impartial", "hospitable", "dust", "naughty", "extra-large", "tacky", "produce", "committee", "fuzzy", "judicious", "nebulous", "stick", "ear", "copy", "friendly", "press", "distinct", "vegetable", "upset", "venomous", "statement", "sulky", "spell", "x-ray", "square", "taste", "great", "thumb", "adjoining", "chilly", "test", "ancient", "green", "badge", "work", "repeat", "free", "elderly", "doctor", "difficult", "grubby", "approval", "turn", "vivacious", "thundering", "cherries", "rest", "plan", "crime", "sticks", "wealthy", "phone", "suspend", "gullible", "fence", "note", "wall", "interest", "coil", "jump", "enchanted", "funny", "racial", "greasy", "polish", "elbow", "smart", "bore", "crowd", "glistening", "oval", "eggs", "nauseating", "detailed", "veil", "coal"]
     var raNum = Math.floor(Math.random() * 70);
     return randomWords[raNum]
@@ -24,14 +34,17 @@ function wordw() {
 
 
 function wordStart() {
-    //update wordGuess array with tiles equal to the length 
-    // of the randomly selected word
+    var wordLength = word.length;
+    var wordL_ = "";
+    var count = wordLength;
+
+    while(count > 0) {
+        wordGuess.push(" _ ");
+        count -= 1;
+    }
 }
 
 function winCountFunc() {
-    /* this function is returning the number of 
-    letters which is used to decide the winning state in 
-    guessWord() function*/
     var num = 0;
     var lettUsed = "";
     var count = word.length;
@@ -53,27 +66,82 @@ function winCountFunc() {
 }
 
 function start() {
-    /* this main function displayed the "mainGame" div and 
-    calls the helper function wordStart(), it also displays 
-    the word progress, number of guesses remaining and the wrong 
-    guesses by user. */
     word = wordw();
     winCount = winCountFunc();
 
-    // allowedGuesses = //set up the allowed guesses
+    if(dif == 1) {
+        guessBomb = word.length + 5;
+    }
 
+    else if(dif == 2) {
+        guessBomb = word.length;
+    }
 
+    else if(dif == 3) {
+        if(word.length % 2 == 0) {
+           guessBomb = word.length / 2;
+        }
+      
+        else {
+           guessBomb = (word.length - 1) / 2;
+        }
+    }
+
+    console.log(word);
+    document.getElementById('mainGame').style.display='block';
+    document.getElementById('startButton').style.display='none';
 
     document.getElementById("question").innerHTML = "Enter your first guess";
 
     wordStart();
 
+    document.getElementById('RRguess').style.display='block';
+    document.getElementById("rightGuess").innerHTML = "word progress: " + wordGuess;
+    document.getElementById("wrongGuess").innerHTML = "Wrong guesses: " + wrongGuess;
+    document.getElementById("guessesLeft").innerHTML = "Guesses remaining: " + guessBomb;
+
+    var x = document.getElementById("guess").maxLength;
+    //document.getElementById("demo").innerHTML = x;
 }
 
 function enterGuess() {
-    // this function will use the helper function isRightOnot()
-    // newCW(), gameLose(), gameWin()
-    
+    var lett = document.getElementById("guess").value;
+    document.getElementById("guess").value = "";
+
+    if (lett.length === 1){
+        var rightOnot = isRightOnot(lett);
+        if (rightOnot == true) {
+
+            NewCW(lett);
+        }
+
+        else {
+            if(!wrongGuess.includes(lett)) {
+                console.log("hi");
+                wrongGuess.push(lett);
+            }
+            guessBomb -= 1;
+        }
+    }
+
+    else if (lett.length < 1) {
+
+    }
+
+    else {
+        guessBomb -= 1;
+    }
+
+    if (guessBomb <= 0) {
+        gameLose()
+    }
+
+    if (winCount <= 0) {
+        gameWin()
+    }
+    document.getElementById("rightGuess").innerHTML = "word progress: " + wordGuess;
+    document.getElementById("wrongGuess").innerHTML = "Wrong guesses: " + wrongGuess;
+    document.getElementById("guessesLeft").innerHTML = "Guesses remaining: " + guessBomb;
 }
 
 function isRightOnot(a) {
@@ -105,15 +173,30 @@ function NewCW(letter) {
 }
 
 function gameLose() {
-
+    document.getElementById('mainGame').style.display='none';
+    document.getElementById('RRguess').style.display='none';
+    document.getElementById('youLose').style.display='block';
+    document.getElementById("correctWordWas").innerHTML = "The correct word was " + word;
 }
 
 function gameWin() {
-
+    document.getElementById('mainGame').style.display='none';
+    document.getElementById('RRguess').style.display='none';
+    document.getElementById('youWin').style.display='block';
 }
 
 function restart() {
+    document.getElementById('mainGame').style.display='none';
+    document.getElementById('RRguess').style.display='none';
+    document.getElementById('youLose').style.display='none';
+    document.getElementById('youWin').style.display='none';
+    document.getElementById('chooseDifficulty').style.display='block';
 
-
-//reset the variables defined on the top of this script
+    word = "";
+    wordGuess = [];
+    wrongGuess = [];
+    guessBomb = 0;
+    winCount = 1;
+    guess = "";
+    dif = 0;
 }
